cb8e33ccdb075a039fbf3e1ee66d166fb9bf4e6d
mukaz4god <muyideenkazeem2011@gmail.com>
2026-06-04 15:25:18 +0100

    revert back to initial code before fix was apply

--- app/vulnerable_asp_net_core/wwwroot/lib/jquery-validation-unobtrusive/jquery.validate.unobtrusive.js
+++ app/vulnerable_asp_net_core/wwwroot/lib/jquery-validation-unobtrusive/jquery.validate.unobtrusive.js
@@ -336,7 +336,12 @@
         return true;
     });
 
-    // Vulnerable code before fix:
+    $jQval.addMethod("regex", function (value, element, params) {
+        var match;
+        if (this.optional(element)) {
+            return true;
+        }
+
         match = new RegExp(params).exec(value);
         return (match && (match.index === 0) && (match[0].length === value.length));
     });
