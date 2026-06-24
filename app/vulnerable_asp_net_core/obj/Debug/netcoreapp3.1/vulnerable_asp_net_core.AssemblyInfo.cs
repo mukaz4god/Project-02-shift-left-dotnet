32a330a35301991f63f83f2074d33a7a25cc4bdf
mukaz4god <muyideenkazeem2011@gmail.com>
2026-06-24 11:41:17 +0100

    update milestone

--- app/vulnerable_asp_net_core/obj/Debug/netcoreapp3.1/vulnerable_asp_net_core.AssemblyInfo.cs
+++ app/vulnerable_asp_net_core/obj/Debug/netcoreapp3.1/vulnerable_asp_net_core.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("vulnerable_asp_net_core")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+aef026e392fb5f1a311c01b3266f634a00520f1e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+955e7bf3e101c3fa5c8b1fad1d300e965ed07b57")]
 [assembly: System.Reflection.AssemblyProductAttribute("vulnerable_asp_net_core")]
 [assembly: System.Reflection.AssemblyTitleAttribute("vulnerable_asp_net_core")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
